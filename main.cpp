8ffee614384f470f465a06da3b8ad0589f23c5ee
lawruixi <52446119+lawruixi@users.noreply.github.com>
2019-12-30 08:35:12 +0800

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+//Random cpp program included for testing purposes
+
 #include <iostream>
 
 using namespace std;
 
 int main(){
     cout << "Hello World!" << endl;
+    cout << "Testing RX's makefile program" << endl;
     return 0;
 }
